49b360cf81ab640984cf81c12d1a58f4707e8329
Andre Salvadeo <andresalvadeo@hotmail.com>
2020-05-18 20:30:22 -0300

    primeiros comandos

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('Olá mundo!');
+import { program as app } from 'commander';
+import * as pkg from './version.json';
+
+app.version(pkg.version);
+
+app
+    .command('add [todo]')
+    .description('Adiciona um to-do')
+    .action((todo) => {
+        console.log(todo);
+    });
+
+app.parse(process.argv);
